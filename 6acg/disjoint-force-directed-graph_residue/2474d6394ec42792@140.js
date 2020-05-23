b444a8f258d4bca9a1489585453ad574bcf878d4
Pranav Ballaney <ballaneypranav@gmail.com>
2020-05-23 19:44:29 +0530

    test

--- 6acg/disjoint-force-directed-graph_residue/2474d6394ec42792@140.js
+++ 6acg/disjoint-force-directed-graph_residue/2474d6394ec42792@140.js
@@ -61,7 +61,7 @@ When using [D3’s force layout](https://github.com/d3/d3-force) with a disjoint
 }
 );
   main.variable(observer("data")).define("data", ["FileAttachment"], function(FileAttachment){return(
-FileAttachment("residue_interactions.jso").json()
+FileAttachment("residue_interactions.json").json()
 )});
   main.variable(observer("height")).define("height", function(){return(
 680
