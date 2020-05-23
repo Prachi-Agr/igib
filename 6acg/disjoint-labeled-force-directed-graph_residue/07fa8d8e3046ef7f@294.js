baa1f5660aea02cf5367107863f3c2ce5d828411
Pranav Ballaney <ballaneypranav@gmail.com>
2020-05-23 22:59:49 +0530

    Update force parameters

--- 6acg/disjoint-labeled-force-directed-graph_residue/07fa8d8e3046ef7f@294.js
+++ 6acg/disjoint-labeled-force-directed-graph_residue/07fa8d8e3046ef7f@294.js
@@ -19,9 +19,10 @@ md`# Disjoint Labeled Force-Directed Graph with Text
   const nodes = data.nodes.map(d => Object.create(d));
   const simulation = d3.forceSimulation(nodes)
       .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody())
+      .force("charge", d3.forceManyBody()
+          .strength(-50))
       .force("x", d3.forceX()
-                    .strength(0.1))
+          .strength(0.1))
       .force("y", d3.forceY());
   
   const svg = d3.select(DOM.svg(width, height))
