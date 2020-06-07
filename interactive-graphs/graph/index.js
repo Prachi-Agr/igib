c2eb9a3d9b67c22c7f44e497273ca91cce009b24
Pranav Ballaney <ballaneypranav@gmail.com>
2020-06-07 12:35:08 +0530

    Towards making a custom fdn

--- interactive-graphs/graph/index.js
+++ interactive-graphs/graph/index.js
@@ -1,14 +1,58 @@
-data = d3.json('d3.min.js');
+data_2ajf = d3.json('https://raw.githubusercontent.com/ballaneypranav/igib/master/interactive-graphs/graph/2AJF.json');
 
-let divSelection = d3.select("body") 
+let leftSelection = d3.select("#left")
     .selectAll("div");
 
- divSelection
-    .data(videoData)
-    .enter()
-  .append('div')
-    .text(function(d) { 
-       return d.title + ": " + d.amount + " views";
-	  })
-    .attr("class", "bar")
-    .style("width", function(d) { return d.amount * 50 + "px"; });
+let rightSelection = d3.select("#right")
+    .selectAll("div");
+
+let chart = {
+    const links = data.links.map(d => Object.create(d));
+    const nodes = data.nodes.map(d => Object.create(d));
+
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => d.id))
+        .force("charge", d3.forceManyBody())
+        .force("x", d3.forceX())
+        .force("y", d3.forceY());
+
+    const svg = d3.create("svg")
+        .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+    const link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("stroke-width", d => Math.sqrt(d.value));
+
+    const node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 5)
+        .attr("fill", color)
+        .call(drag(simulation));
+
+    node.append("title")
+        .text(d => d.id);
+
+    simulation.on("tick", () => {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+    });
+
+    invalidation.then(() => simulation.stop());
+
+    return svg.node();
+}
